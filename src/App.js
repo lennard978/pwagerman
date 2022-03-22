2f776e418c7121c8bf5c38078702fb4e7cecd633
Lennard <shparavalo78nenad@yahoo.com>
2022-03-22 09:58:02 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { Lesson1 } from "./components/Data/data";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const list = Lesson1.map((item) => {
+    return (
+      <div
+        key={uuidv4()}
+        className="flex justify-between p-1 flex-row border-2 border-slate-800 m-1 p-4 cursor-pointer"
+        onClick={() => speak({ rate: 0.5, pitch: 0, voice, text: item.german })}
+      >
+        <p className="font-bold text-left">{item.german}</p>
+        <p className="text-right italic">{item.english}</p>
+      </div>
+    );
+  });
+  const { speak, voices } = useSpeechSynthesis();
+  const voice = voices.find(({ lang }) => lang.startsWith("de"));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-100">
+      <h1 className="flex justify-center font-serif font-bold uppercase p-3">
+        Lesson 1
+      </h1>
+      <div>{list}</div>
     </div>
   );
 }
