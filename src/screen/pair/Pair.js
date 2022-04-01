f3d0dc437d9a95916066cdd591d8fdbb995abd0c
Lennard <shparavalo78nenad@yahoo.com>
2022-04-01 10:16:55 +0200

    Create Write

--- src/screen/pair/Pair.js
+++ src/screen/pair/Pair.js
@@ -20,17 +20,13 @@ export const Pair = ({ data }) => {
 
   const checkGerman = (item) => {
     germanResultNew = item;
-    console.log(germanResultNew);
   };
   const checkEnglish = (item) => {
     englishResultNew = item;
-    console.log(englishResultNew);
     checkCorrect();
   };
 
   const checkCorrect = () => {
-    console.log(germanResultNew);
-    console.log(englishResultNew);
     if (germanResultNew.german === englishResultNew.german) {
       setGerman(german.filter((item) => item !== germanResultNew));
       setEnglish(english.filter((item) => item !== englishResultNew));
@@ -56,16 +52,14 @@ export const Pair = ({ data }) => {
     <Container>
       <Title title={`Pair ${number + 1}`} />
       <Row>{germanList}</Row>
-      {/* <Check onClick={() => checkCorrect()}>Check</Check> */}
       <Row>{englishList}</Row>
-      {/* <Check to="/choosepair">Go Back</Check> */}
     </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: space-around;
   padding-top: 2.7rem;
   padding-bottom: 4rem;
@@ -73,15 +67,13 @@ const Container = styled.div`
 
 const Row = styled.div`
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
 `;
 
 const Btn = styled.button`
   text-transform: capitalize;
-  font-size: 1rem;
+  font-size: 0.8rem;
   font-weight: 400;
   color: rgba(57, 255, 20, 1);
   margin: 5px;
@@ -92,35 +84,12 @@ const Btn = styled.button`
   border-top: 1px solid #243b50;
   border-left: 1px solid #243b50;
   box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.3);
-  background: transparent;
+  background: linear-gradient(to top, #243b50, #141e30);
   text-decoration: none;
   border-radius: 0.2rem;
+  cursor: pointer;
   &:hover {
-    border-bottom: 1px solid rgba(57, 255, 20, 1);
-    border-left: 1px solid rgba(57, 255, 20, 1);
-    transition: all 0.5s ease;
-  }
-`;
-
-const Check = styled.button`
-  text-transform: capitalize;
-  font-size: 0.6rem;
-  font-weight: 400;
-  color: rgba(57, 255, 20, 1);
-  margin: 5px;
-  padding-block: 0.5rem;
-  padding-inline: 2rem;
-  border-bottom: 1px solid #141e30;
-  border-right: 1px solid #141e30;
-  border-top: 1px solid #243b50;
-  border-left: 1px solid #243b50;
-  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.3);
-  background: transparent;
-  text-decoration: none;
-  border-radius: 0.2rem;
-  &:hover {
-    border-bottom: 1px solid rgba(57, 255, 20, 1);
-    border-left: 1px solid rgba(57, 255, 20, 1);
+    border: 1px inset rgba(57, 255, 20, 1);
     transition: all 0.5s ease;
   }
 `;
--- src/screen/write/Write.js
+++ src/screen/write/Write.js
@@ -1,23 +1,110 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
-import SoundButton from "../../components/SoundButton";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { Title } from "../../components/Title";
 
 export const Write = ({ data }) => {
   const { userId } = useParams();
   const number = Number(userId);
+
+  const wordList = data[userId].map((item) => {
+    return item;
+  });
+
+  const [count, setCount] = useState(0);
+  const [englishWord, setEnglishWord] = useState(wordList[count].english);
+  const [germanWord, setGermanWord] = useState(wordList[count].german);
+  const shuffle = (v) => [...v].sort((_) => Math.random() - 0.5).join("");
+  const shuffleWord = shuffle(germanWord.split(""));
+  const [german, setGerman] = useState([...shuffleWord]);
+
   return (
-    <div>
+    <Container>
       <Title title={`Write ${number + 1}`} />
-      {data[userId].map((item, index) => {
-        return (
-          <SoundButton key={index} german={item.german}>
-            <p>{item.german}</p>
-            <p>{item.english}</p>
-          </SoundButton>
-        );
-      })}
-      <Link to="/choosewrite">Back to Write</Link>
-    </div>
+      <Row>
+        <H2>{englishWord}</H2>
+      </Row>
+      <Row>
+        {german.map((item, index) => {
+          return <GermanLetter key={index}>{item}</GermanLetter>;
+        })}
+      </Row>
+      <Button>Next</Button>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-top: 2.7rem;
+  padding-bottom: 4rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 2.7rem;
+  padding-bottom: 4rem;
+  margin-inline: 1rem;
+`;
+
+const H2 = styled.h2`
+  color: white;
+`;
+
+const GermanLetter = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1rem;
+  color: white;
+  padding-block: 0.5rem;
+  padding-inline: 1rem;
+  border-bottom: 1px solid #141e30;
+  border-right: 1px solid #141e30;
+  border-top: 1px solid #243b50;
+  border-left: 1px solid #243b50;
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(to top, #243b50, #141e30);
+  text-decoration: none;
+  border-radius: 0.2rem;
+  margin: 0.2rem;
+  text-transform: uppercase;
+  cursor: pointer;
+  &:hover {
+    border: 1px inset rgba(57, 255, 20, 1);
+    transition: all 0.5s ease;
+  }
+`;
+
+const Button = styled.button`
+  text-transform: capitalize;
+  font-size: 0.8rem;
+  font-weight: 400;
+  color: white;
+  margin: 5px;
+  padding-block: 0.5rem;
+  padding-inline: 2rem;
+  border-bottom: 1px solid #141e30;
+  border-right: 1px solid #141e30;
+  border-top: 1px solid #243b50;
+  border-left: 1px solid #243b50;
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(to top, #243b50, #141e30);
+  text-decoration: none;
+  border-radius: 0.2rem;
+  cursor: pointer;
+  width: 3rem;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  &:hover {
+    border-bottom: 1px inset rgba(57, 255, 20, 1);
+    transition: all 0.5s ease;
+    color: rgba(57, 255, 20, 1);
+  }
+`;
